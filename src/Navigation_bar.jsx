e3c99db8e5d9b71b9dd5a7ccbcbc211efe9c1e63
Vince <vincent.ugwah@gmail.com>
2025-12-30 05:46:26 -0500

    Add concise, recruiter-friendly README with project overview and setup instructions

--- src/Navigation_bar.jsx
+++ src/Navigation_bar.jsx
@@ -11,7 +11,6 @@ function Navigation_bar() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about"> About </Link></li>
-            <li><Link to="/contact">Contact</Link></li>
             <li><Link to="/booking">Booking</Link></li>
         </ul>
       </nav>
